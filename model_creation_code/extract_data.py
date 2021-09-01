35dbdbcaa88cc4d7a116a9d398c37ed4bac31e69
Aaron Reuven <roni.reuven@gmail.com>
2021-09-01 08:32:39 +0300

    added directories

--- model_creation_code/extract_data.py
+++ model_creation_code/extract_data.py
@@ -12,13 +12,13 @@ def get_data(list_of_classes):
         storage_client = storage.Client.create_anonymous_client()
         bucket = storage_client.bucket(BUCKET)
         blob = bucket.blob(OBJECT_PATH + cls + FILE_EXT)
-        blob.download_to_filename(cls + FILE_EXT)
+        blob.download_to_filename(DATA_PATH + cls + FILE_EXT)
 
 
 def mush_into_dataframe(list_of_classes):
     dfs = []
     for cls in list_of_classes:
-        array = np.load(cls + FILE_EXT)
+        array = np.load(DATA_PATH + cls + FILE_EXT)
         df = pd.DataFrame(array)
         df['label'] = np.full((array.shape[0], 1), cls)
         dfs.append(df)
