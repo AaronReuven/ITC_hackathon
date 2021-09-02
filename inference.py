06113b188347a5ca7e8e7dd8adb0db7bfc9379d5
Aaron Reuven <roni.reuven@gmail.com>
2021-09-02 11:51:38 +0300

    fix server

--- inference.py
+++ inference.py
@@ -11,14 +11,36 @@ import json
 app = Flask(__name__)
 
 
+def read_model(file):
+    """
+    reads the model from file
+    :param file: file to read the model from
+    :return: trained model from file
+    """
+    # print('Downloading model')
+    # if not os.path.exists(DOWNLOAD_MODEL_PATH):
+    #     os.mkdir(DOWNLOAD_MODEL_PATH)
+    # s3 = boto3.resource('s3', aws_access_key_id=ACCESS_ID, aws_secret_access_key=ACCESS_KEY)
+    # s3.meta.client.download_file(DRAW_BUCKET, S3_MODEL_FILE, os.path.abspath(DOWNLOAD_MODEL_PATH) + '/' + S3_MODEL_FILE)
+    # print('finished downloading')
+    model = None
+    # with open(os.path.abspath(DOWNLOAD_MODEL_PATH) + '/' + S3_MODEL_FILE, 'rb') as f:
+    with open(file, 'rb') as f:
+        model = pickle.load(f)
+    return model
+
+
+model = read_model(os.path.abspath(MODEL_FILE))
+
+
 @app.route('/predict_drawing', methods=['POST'])
 def predict_bulk():
     """
     runs the predictions on the model of dataframe passed as json
     :return: json of predictions
     """
-    if 'model' not in globals():
-        model = read_model(os.path.abspath(MODEL_FILE))
+    # if 'model' not in globals():
+    #     model = read_model(os.path.abspath(MODEL_FILE))
     predict_data = json.loads(flask.request.get_json())
     # return predict_data
     # return 'can load'
@@ -43,33 +65,14 @@ def hi():
     return 'hi'
 
 
-def read_model(file):
-    """
-    reads the model from file
-    :param file: file to read the model from
-    :return: trained model from file
-    """
-    # print('Downloading model')
-    # if not os.path.exists(DOWNLOAD_MODEL_PATH):
-    #     os.mkdir(DOWNLOAD_MODEL_PATH)
-    # s3 = boto3.resource('s3', aws_access_key_id=ACCESS_ID, aws_secret_access_key=ACCESS_KEY)
-    # s3.meta.client.download_file(DRAW_BUCKET, S3_MODEL_FILE, os.path.abspath(DOWNLOAD_MODEL_PATH) + '/' + S3_MODEL_FILE)
-    # print('finished downloading')
-    model = None
-    # with open(os.path.abspath(DOWNLOAD_MODEL_PATH) + '/' + S3_MODEL_FILE, 'rb') as f:
-    with open(file, 'rb') as f:
-        model = pickle.load(f)
-    return model
-
-
-def main():
-    model = read_model(os.path.abspath(MODEL_FILE))
-    return model
+# def main():
+#     model = read_model(os.path.abspath(MODEL_FILE))
+#     return model
     # pass
 
 
 if __name__ == '__main__':
-    model = main()
+    # model = main()
     port = os.environ.get('PORT')
     if port:
         # 'PORT' variable exists - running on Heroku, listen on external IP and on given by Heroku port
