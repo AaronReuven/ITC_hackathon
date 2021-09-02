f2dc2895c30cf10f786dfd81c086593da0bafaba
Aaron Reuven <roni.reuven@gmail.com>
2021-09-02 14:49:13 +0300

    new code

--- inference.py
+++ inference.py
@@ -1,10 +1,14 @@
 import base64
+import io
 import pickle
+import string
+
 import flask
 import os
 # from boto3.session import Session
 import boto3
 import numpy as np
+from numpy import resize
 
 from configurations.config import *
 import pandas as pd
@@ -40,39 +44,69 @@ def read_model(file):
 
 model = read_model(os.path.abspath(MODEL_FILE))
 
+@app.route("/predict_drawing", methods=["POST"])
+@cross_origin(supports_credentials=True)
+def json():
+    if flask.request.is_json:
+        try:
+            req = flask.request.get_json()
+            dic_letters = {i: letter for i, letter in enumerate(string.ascii_uppercase)}
+            row = fix_input_image(req.get("data"))
 
-@app.route('/predict_drawing', methods=['POST'])
-@cross_origin()
-def predict_bulk():
-    """
-    runs the predictions on the model of dataframe passed as json
-    :return: json of predictions
-    """
-    image_b64 = flask.request.args.get('b_str')
-    # print(r)
-    # predict_data = json.loads(r)
-    # print(predict_data)
-    # b_srt = r[22:]
-    # base64_img_bytes = r.encode('utf-8')
-    new_image = Image.open(BytesIO(base64.b64decode(image_b64))).convert('1').resize((28, 28))
-    # with open('decoded_image.png', 'wb') as file_to_save:
-    # decoded_image_data = base64.b64decode(base64_img_bytes)
-        # file_to_save.write(decoded_image_data)
-    # Image.open(BytesIO(decoded_image_data))
-    # img = Image.open(BytesIO(decoded_image_data)).convert('1').resize((28, 28))
-    new_image.save('x_pred.jpg')
+            # letter, certain = dic_letters[clf.predict(row)[0]], clf.predict_proba(np.max(row))
 
-    x_pred = read_new_image('x_pred.jpg')
-    predict_df = pd.DataFrame(np.array([x_pred]))
+            return flask.make_response(flask.jsonify({'letter': row, 'certain': 'certain'}), 200)
+        except Exception as ex:
+            return flask.make_response(flask.jsonify({'error': ex}), 500)
 
-    # r = flask.request.data
-    # nparr = np.fromstring(r,np.uint8)
-    # predict_df = pd.DataFrame(nparr)
-    results = model.predict_proba(predict_df)
-    result = {RESULT_JSON_TAG: results.tolist()}
-    return flask.jsonify(result)
+        # return make_response(jsonify({'error': 'asa'}), 500)
+    else:
+        return flask.make_response(flask.jsonify({"message": "No JSON"}), 400)
+# @app.route('/predict_drawing', methods=['POST'])
+# @cross_origin()
+# def predict_bulk():
+#     """
+#     runs the predictions on the model of dataframe passed as json
+#     :return: json of predictions
+#     """
+#     image_b64 = flask.request.args.get('b_str')
+#     # print(r)
+#     # predict_data = json.loads(r)
+#     # print(predict_data)
+#     # b_srt = r[22:]
+#     # base64_img_bytes = r.encode('utf-8')
+#     new_image = Image.open(BytesIO(base64.b64decode(image_b64))).convert('1').resize((28, 28))
+#     # with open('decoded_image.png', 'wb') as file_to_save:
+#     # decoded_image_data = base64.b64decode(base64_img_bytes)
+#         # file_to_save.write(decoded_image_data)
+#     # Image.open(BytesIO(decoded_image_data))
+#     # img = Image.open(BytesIO(decoded_image_data)).convert('1').resize((28, 28))
+#     new_image.save('x_pred.jpg')
+#
+#     x_pred = read_new_image('x_pred.jpg')
+#     predict_df = pd.DataFrame(np.array([x_pred]))
+#
+#     # r = flask.request.data
+#     # nparr = np.fromstring(r,np.uint8)
+#     # predict_df = pd.DataFrame(nparr)
+#     results = model.predict_proba(predict_df)
+#     result = {RESULT_JSON_TAG: results.tolist()}
+#     return flask.jsonify(result)
 
+def fix_input_image(str_img):
+    image = Image.open(io.BytesIO(base64.urlsafe_b64decode(str_img)))
+    image_np = np.array(image)
+    # image_np = rgb2gray(rgba2rgb(image_np))
+    image = resize(image_np, (28, 28))
 
+    # squarer2 = lambda t: 0 if t == 255 else t
+    # squarer = lambda t: (int((t + 1) * 255 / 2) - 255) * -1
+    # vfunc = np.vectorize(squarer)
+    # image = vfunc(image)
+    # vfunc = np.vectorize(squarer2)
+    # image = vfunc(image)
+
+    return image.reshape((1, -1))
 # @app.route('/predict_drawing')
 # def predict():
 #     """
